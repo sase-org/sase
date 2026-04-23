3b625924d2c2bf2d8b078ced8779f2e7acf20f69
Bryan Bugyi <bryanbugyi34@gmail.com>
2026-04-23 16:09:09 -0400

    Revert "Reapply "Reapply "feat: Add bold centered startup loading banner to `sase ace` TUI"""
    This reverts commit 8b355229bce3113c741bd2f458afd81bfb795164.

--- src/sase/ace/tui/actions/axe_display.py
+++ src/sase/ace/tui/actions/axe_display.py
@@ -160,7 +160,6 @@ class AxeDisplayMixin:
                 info_panel.set_loading(False)
             except Exception:
                 pass
-            self._maybe_hide_startup_banner()  # type: ignore[attr-defined]
 
         self.axe_running = data.axe_running
 
