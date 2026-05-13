158b5b33e0969047100b3e763b4542ad00942008
Bryan Bugyi <bryanbugyi34@gmail.com>
2026-05-13 14:42:16 -0400

    chore: Fix pyvision pragma

--- src/sase/core/wire.py
+++ src/sase/core/wire.py
@@ -152,7 +152,7 @@ class ChangeSpecWire:
     deltas: list[DeltaWire] = field(default_factory=list)
 
 
-# pyvision: ../sase-core/crates/sase_core/src/wire.rs
+# pyvision: https://github.com/sase-org/sase-core.git
 @dataclass
 class ParseErrorWire:
     """Structured error a Rust parser may emit instead of a ChangeSpecWire."""
